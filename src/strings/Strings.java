7975615b28c71b750ef9da5ac163937556519f18
unknown <haratheavula2003@gmail.com>
2024-09-12 18:57:56 +0530

    adding src only

---
+++ src/strings/Strings.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		//String name="Arthi";
+		//System.out.println(name);
+		//String name=new String("Arthi");
+		//System.out.println(name);
+	   String str1="Arthi ";
+	   String str2="krish";
+	 //System.out.println(str1.length());
+	   System.out.println(str1.concat(str2));  
+	}
+}
